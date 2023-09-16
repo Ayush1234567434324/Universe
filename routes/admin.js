a5d893f06fdb9bd2661ad2fa0af49686e61f664b
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-09-16 18:00:09 +0530

    your message

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Manga = require('../models/Admin') 
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors middleware
-
 router.use(bodyParser.json());
-
-// Use cors middleware
-router.use(cors());
 
 
 router.get('/',(req,res)=>{
@@ -18,8 +13,20 @@ router.get('/',(req,res)=>{
   res.json(obj);});
 
 
+
+
+  
+
 router.post('/submit-data', (req, res) => {
-   
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   const formData = req.body;
   
   // Assuming you have a Mongoose model Manga
