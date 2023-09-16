ef3007860112443f3669d6eedbb5867090bfa697
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-09-16 17:37:19 +0530

    your message

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Set the allowed origin(s)
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/'); // Set the allowed origin(s)
     // Other CORS headers may also be necessary
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
