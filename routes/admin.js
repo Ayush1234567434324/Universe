ee8a442fe748f16cf45e4b62c581c2327fbc036e
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-09-16 23:56:14 +0530

    your message

--- routes/admin.js
+++ routes/admin.js
@@ -18,30 +18,11 @@ router.get('/',(req,res)=>{
   
 
 router.post('/submit-data', (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+   
   const formData = req.body;
   
   // Assuming you have a Mongoose model Manga
-  const manga = new Manga(formData);
-  
-  // Save the data to the MongoDB database
-  manga.save()
-    .then(() => {
-      console.log('Data saved successfully:', formData);
-      res.json({ message: 'Data received and saved successfully' });
-    })
-    .catch((error) => {
-      console.error('Error saving data:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    });
+    res.send(formData)
 });
 
 module.exports = router;
