2858996e0969381644a6188d378b33532e737f5a
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-09-16 17:35:04 +0530

    your message

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ connectDB();
 app.use(express.json())
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Set the allowed origin(s)
+    // Other CORS headers may also be necessary
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 
 
