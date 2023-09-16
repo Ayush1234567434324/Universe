1ff621de37a557dc4d8945756b4a73f74d6ace6a
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-09-16 13:16:20 +0530

    your message

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ connectDB();
 
 app.use(express.json())
 app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
+
+
 app.use('/api',require('./routes/admin'))
+
 const PORT = process.env.PORT
 app.listen(PORT,
     ()=>{
