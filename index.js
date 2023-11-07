2cf732b306d62af1282e0e4b3ed07b26a59d32c4
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-11-08 01:15:00 +0530

    uou

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+const request = require('request');
 require('dotenv').config()
 const connectDB = require('./db')
 
@@ -10,32 +11,25 @@ app.use(express.json())
 app.use(cors());
 
 
-const request = require('request');
+
 
 app.use('/pdf', (req, res) => {
-  // Get the 'id' parameter from the request URL
-  const id = '10TI5xrc4heHV1bFeeXUPlyMNEBfs1WCX';
-
-  // Create the Google Drive link with the provided 'id'
-  const googleDriveLink = `https://drive.google.com/file/d/${id}/view`;
+  const googleDriveLink = 'https://drive.google.com/file/d/1oWm--awbX0cYscaG99fFKwHwrzV7vwfs/view';
 
   // Extract the file ID from the Google Drive link
   const fileId = googleDriveLink.match(/\/d\/(.+?)\//);
   if (fileId && fileId[1]) {
-      const directDownloadLink = `https://drive.google.com/uc?id=${fileId[1]}`;
-
-      // Set the response headers to indicate a PDF file
-      res.setHeader('Content-Type', 'application/pdf');
-
-      // Pipe the direct download link to the response
-      request(directDownloadLink).pipe(res);
+    const directDownloadLink = `https://drive.google.com/uc?id=${fileId[1]}`;
+    
+    // Set the response headers to indicate a PDF file
+    res.setHeader('Content-Type', 'application/pdf');
+    
+    // Pipe the direct download link to the response
+    request(directDownloadLink).pipe(res);
   } else {
-      res.status(500).send('Invalid Google Drive link');
+    res.status(500).send('Invalid Google Drive link');
   }
 });
-
-  
- 
   
   
 app.use('/api',require('./routes/admin'))
