1d98ea30cc01ec039ddba4544cf9c42abe06e0dc
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-11-08 00:58:59 +0530

    uou

--- index.js
+++ index.js
@@ -12,27 +12,28 @@ app.use(cors());
 
 const request = require('request');
 
-app.use('/pdf/:id', (req, res) => {
-    // Get the 'id' parameter from the request URL
-    const id = req.params.id;
-  
-    // Create the Google Drive link with the provided 'id'
-    const googleDriveLink = `https://drive.google.com/file/d/${id}/view`;
-  
-    // Set the response headers to indicate a PDF file
-    
-  
-    // Extract the file ID from the Google Drive link
-    const fileId = googleDriveLink.match(/\/d\/(.+?)\//);
-    if (fileId && fileId[1]) {
+app.get('/pdf/:id', (req, res) => {
+  // Get the 'id' parameter from the request URL
+  const id = req.params.id;
+
+  // Create the Google Drive link with the provided 'id'
+  const googleDriveLink = `https://drive.google.com/file/d/${id}/view`;
+
+  // Extract the file ID from the Google Drive link
+  const fileId = googleDriveLink.match(/\/d\/(.+?)\//);
+  if (fileId && fileId[1]) {
       const directDownloadLink = `https://drive.google.com/uc?id=${fileId[1]}`;
-      
+
+      // Set the response headers to indicate a PDF file
+      res.setHeader('Content-Type', 'application/pdf');
+
       // Pipe the direct download link to the response
       request(directDownloadLink).pipe(res);
-    } else {
+  } else {
       res.status(500).send('Invalid Google Drive link');
-    }
+  }
 });
+
   
  
   
