ded7e1d2c7fd6fb9baa76d6265bace52f60c1bd8
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2023-11-14 14:37:43 +0530

    uou

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json())
 app.use(cors());
 
 const axios = require('axios');
+const { PDFDocument } = require('pdf-lib');
 const rangeParser = require('range-parser');
 const { pipeline } = require('stream');
 
@@ -17,49 +18,48 @@ app.get('/pdf/:fileId', async (req, res) => {
   const fileId = req.params.fileId;
 
   try {
+    // Download the PDF file from Google Drive
     const response = await axios({
       method: 'GET',
       url: `https://drive.google.com/uc?export=download&id=${fileId}`,
-      responseType: 'stream',
+      responseType: 'arraybuffer', // Make sure to request the file as an array buffer
     });
 
-    const fileSize = parseInt(response.headers['content-length'], 10);
-    const range = req.headers.range;
+    // Create a PDF document from the downloaded buffer
+    const pdfDoc = await PDFDocument.load(response.data);
 
-    if (range) {
-      const parts = rangeParser(fileSize, range);
+    // Convert the PDF document to a buffer
+    const pdfBuffer = await pdfDoc.save();
 
-      if (parts) {
-        const [start, end] = parts[0];
-        res.status(206).set({
-          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': end - start + 1,
-          'Content-Type': response.headers['content-type'],
-        });
+    // Set headers for the response
+    res.setHeader('Accept-Ranges', 'bytes');
+    res.setHeader('Content-Type', 'application/pdf');
 
-        pipeline(response.data, res, { start, end }, (error) => {
-          if (error) {
-            console.error('Pipeline error:', error);
-            res.status(500).send('Error occurred while piping the file');
-          }
-        });
+    // Check the file size
+    const fileSize = pdfBuffer.length;
 
-        return;
+    // If the file size exceeds 4 MB, split it into chunks
+    if (fileSize > 4 * 1024 * 1024) {
+      const chunks = Math.ceil(fileSize / (4 * 1024 * 1024));
+      res.setHeader('Content-Range', `bytes 0-${fileSize - 1}/${fileSize}`);
+      res.status(206); // Partial Content
+
+      for (let i = 0; i < chunks; i++) {
+        const start = i * (4 * 1024 * 1024);
+        const end = Math.min((i + 1) * (4 * 1024 * 1024) - 1, fileSize - 1);
+
+        // Send each chunk
+        res.write(pdfBuffer.slice(start, end + 1));
       }
+    } else {
+      // If the file size is below 4 MB, send the entire file
+      res.status(200);
+      res.setHeader('Content-Length', fileSize);
+      res.write(pdfBuffer);
     }
 
-    res.status(200).set({
-      'Content-Length': fileSize,
-      'Content-Type': response.headers['content-type'],
-    });
-
-    pipeline(response.data, res, (error) => {
-      if (error) {
-        console.error('Pipeline error:', error);
-        res.status(500).send('Error occurred while piping the file');
-      }
-    });
+    // End the response
+    res.end();
   } catch (error) {
     console.error(error);
     res.status(500).send('Error occurred while downloading the file');
